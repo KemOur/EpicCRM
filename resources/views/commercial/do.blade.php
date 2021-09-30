997e2b0875c6770a76e4fa11202ba8effa8ad4d3
Junior <duartejusanca@gmail.com>
2021-09-30 13:07:45 +0200

    icons back-to-last page added

--- resources/views/commercial/do.blade.php
+++ resources/views/commercial/do.blade.php
@@ -13,7 +13,20 @@
                     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 bg-white border-b border-gray-200">
                             @if (auth()->check() && auth()->user()->id)
-                                <h5>Espace de travail {{auth()->user()->name}}</h5>
+                                <a href="{{ route('commercial.dashboard') }}">
+                                <div class="d-flex justify-content-between">
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                                        </svg>
+                                    </div>
+                                    <div>
+                                        @if (auth()->check() && auth()->user()->id)
+                                            <h5>Espace de travail {{auth()->user()->name}}</h5>
+                                        @else
+                                        @endif
+                                    </div>
+                                </div>
                             @else
                             @endif
                         </div>
